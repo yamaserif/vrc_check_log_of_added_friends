f6f594f87ff173557618912649be3e18c1b9f1f9
yamaserif <62947068+yamaserif@users.noreply.github.com>
2024-12-03 03:45:22 +0900

    重複周りの処理修正

--- run.ps1
+++ run.ps1
@@ -13,22 +13,32 @@ $grepPath = $FileDialog.FileName
 $grepPath
 $FileDialog.Dispose()
 
+Write-Output "◆ユーザーのJoinデータログ"
 # "OnPlayerJoined"のログにユーザーIDがあるっぽいので、抽出しておく
-$userIdLogs = [regex]::Matches((Get-Content -Raw $grepPath -encoding utf8), "OnPlayerJoined\s[^\r\n]*?\s\(usr_[^\r\n]*?\)").Value
+$userIdLogs = [regex]::Matches((Get-Content -Raw $grepPath -encoding utf8), "OnPlayerJoined\s[^\r\n]*?\s\(usr_[^\r\n]*?\)").Value | Select-Object -Unique
 $userIdLogs
 
+Write-Output "◆フレンドを追加したと思われるログ(想定)"
 # フレンド追加時に"is no longer Muted"のログ追加されてるっぽいので、それを元に名前を含む部分を切り出す
-$friendsData = [regex]::Matches((Get-Content -Raw $grepPath -encoding utf8), "\[ModerationManager\]\s[^\r\n]*?\sis\sno\slonger\sMuted") | Get-Unique
+$friendsData = [regex]::Matches((Get-Content -Raw $grepPath -encoding utf8), "\[ModerationManager\]\s[^\r\n]*?\sis\sno\slonger\sMuted").Value | Select-Object -Unique
+$friendsData
+
+Write-Output "◆抽出したフレンドのユーザ情報"
 foreach($friend in $friendsData){
-    Write-Host --------------------------
-    $friend.Value
+    Write-Output --------------------------
+    $friend
 
     # フレンド名を取得
-    $friendName = $friend.Value.Substring(20, $friend.Value.length - 39)
+    $friendName = $friend.Substring(20, $friend.length - 39)
     $friendName
 
     # ユーザIDを取得
     $userIdLog = $userIdLogs | Where-Object {$_ -like "*$friendName*"}
+    if($userIdLog -is [array])
+    {
+        $userIdLog = $userIdLog[0]
+    }
+    $userIdLog
     $userId = $userIdLog.Substring($userIdLog.length - 41, 40)
     $userId
 
